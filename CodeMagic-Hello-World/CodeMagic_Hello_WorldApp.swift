164d6b0e497b26af52d34dabac18e98ca96c99e7
Eugeniu Racila <eugeniu.racila@gmail.com>
2022-11-27 15:37:28 +0100

    feat: initial commit

---
+++ CodeMagic-Hello-World/CodeMagic_Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeMagic_Hello_WorldApp.swift
+//  CodeMagic-Hello-World
+//
+//  Created by Eugeniu Racila on 27/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodeMagic_Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeMagic-Hello-World/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CodeMagic-Hello-World
+//
+//  Created by Eugeniu Racila on 27/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
